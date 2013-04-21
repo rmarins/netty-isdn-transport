8648f9e6ce8c6a1174b46845f8b26c2a817fcb09
Rafael Marins <rafa.marins@gmail.com>
2013-04-21 10:06:01 -0300

    Commented out the existings Remote CAPI tests.

--- src/test/java/org/neociclo/capi20/remote/RemoteCapiTest.java
+++ src/test/java/org/neociclo/capi20/remote/RemoteCapiTest.java
@@ -47,80 +47,80 @@ public class RemoteCapiTest {
         this.capi = null;
     }
 
-//    @Test
-//    public void testGetManufacturer() throws Exception {
-//        String manufacturerInfo = capi.getManufacturer(0);
-//        assertNotNull("No manufacturer info returned.", manufacturerInfo);
-//    }
-//
-//    @Test
-//    public void testGetProfile() throws Exception {
-//        byte[] profile = capi.getProfile(0);
-//        assertNotNull("Invalid Capi.getProfile() null response.", profile);
-//        assertEquals("Invalid Capi.getProfile() response size.", 64, profile.length);
-//    }
-//
-//    @Test
-//    public void testGetSerialNumber() throws Exception {
-//        String serialInfo = capi.getSerialNumber(0);
-//        assertNotNull("No serial number returned.", serialInfo);
-//    }
-//
-//    @Test
-//    public void testGetVersion() throws Exception {
-//        int version = capi.getVersion(0);
-//        assertTrue("Bad version info: " + version, version != 0);
-//    }
-//
-//    @Test
-//    public void testIsInstalled() throws Exception {
-//        boolean installed = capi.isInstalled();
-//        assertTrue("Capi not installed.", installed);
-//    }
-//
-//    @Test
-//    public void testRegisterOnly() throws Exception {
-//        int appID = capi.register(3072, 2, 7, 2048);
-//        assertEquals("Initial AppID must be equal to ONE.", 1, appID);
-//    }
-//
-//    @Test
-//    public void testRegisterAndRelease() throws Exception {
-//        int appID = capi.register(3072, 2, 7, 2048);
-//        capi.release(appID);
-//    }
-//
-//    @Test
-//    public void testInfoMessageExchanges() throws Exception {
-//
-//        // CAPI_REGISTER
-//        int appID = capi.register(3072, 2, 7, 2048);
-//
-//        // send INFO_REQ
-//        InfoReq infoReq = new InfoReq();
-//        infoReq.setPlci(new PLCI(1)); // controller 1
-//        infoReq.setAppID(appID);
-//        infoReq.setMessageID(1);
-//        capi.putMessage(appID, infoReq.getRawOctets());
-//
-//        // get back INFO_CONF
-//        capi.waitForSignal(appID);
-//        byte[] rawConf = capi.getMessage(appID);
-//        InfoConf infoConf = new InfoConf(wrappedBuffer(rawConf));
-//
-//        // CAPI_RELEASE
-//        capi.release(appID);
-//
-//        assertEquals("INFO_CONF -> Message AppID mismatch.", appID, infoConf.getAppID());
-//        assertEquals("INFO_CONF -> Message Number mismatch.", 1, infoConf.getMessageID());
-//
-//        assertNotNull("INFO_CONF -> Non null PLCI were expected.", infoConf.getPlci());
-//        assertEquals("INFO_CONF -> PLCI controller were mismatch.", 1, infoConf.getPlci().getController().getController());
-//
-//        assertNotNull("INFO_CONF -> Non null INFO expected.", infoConf.getInfo());
-//        assertEquals("INFO_CONF ->  Transmission of information initiated were expected.", Info.REQUEST_ACCEPTED,
-//                infoConf.getInfo());
-//    }
+    @Test
+    public void testGetManufacturer() throws Exception {
+        String manufacturerInfo = capi.getManufacturer(0);
+        assertNotNull("No manufacturer info returned.", manufacturerInfo);
+    }
+
+    @Test
+    public void testGetProfile() throws Exception {
+        byte[] profile = capi.getProfile(0);
+        assertNotNull("Invalid Capi.getProfile() null response.", profile);
+        assertEquals("Invalid Capi.getProfile() response size.", 64, profile.length);
+    }
+
+    @Test
+    public void testGetSerialNumber() throws Exception {
+        String serialInfo = capi.getSerialNumber(0);
+        assertNotNull("No serial number returned.", serialInfo);
+    }
+
+    @Test
+    public void testGetVersion() throws Exception {
+        int version = capi.getVersion(0);
+        assertTrue("Bad version info: " + version, version != 0);
+    }
+
+    @Test
+    public void testIsInstalled() throws Exception {
+        boolean installed = capi.isInstalled();
+        assertTrue("Capi not installed.", installed);
+    }
+
+    @Test
+    public void testRegisterOnly() throws Exception {
+        int appID = capi.register(3072, 2, 7, 2048);
+        assertEquals("Initial AppID must be equal to ONE.", 1, appID);
+    }
+
+    @Test
+    public void testRegisterAndRelease() throws Exception {
+        int appID = capi.register(3072, 2, 7, 2048);
+        capi.release(appID);
+    }
+
+    @Test
+    public void testInfoMessageExchanges() throws Exception {
+
+        // CAPI_REGISTER
+        int appID = capi.register(3072, 2, 7, 2048);
+
+        // send INFO_REQ
+        InfoReq infoReq = new InfoReq();
+        infoReq.setPlci(new PLCI(1)); // controller 1
+        infoReq.setAppID(appID);
+        infoReq.setMessageID(1);
+        capi.putMessage(appID, infoReq.getRawOctets());
+
+        // get back INFO_CONF
+        capi.waitForSignal(appID);
+        byte[] rawConf = capi.getMessage(appID);
+        InfoConf infoConf = new InfoConf(wrappedBuffer(rawConf));
+
+        // CAPI_RELEASE
+        capi.release(appID);
+
+        assertEquals("INFO_CONF -> Message AppID mismatch.", appID, infoConf.getAppID());
+        assertEquals("INFO_CONF -> Message Number mismatch.", 1, infoConf.getMessageID());
+
+        assertNotNull("INFO_CONF -> Non null PLCI were expected.", infoConf.getPlci());
+        assertEquals("INFO_CONF -> PLCI controller were mismatch.", 1, infoConf.getPlci().getController().getController());
+
+        assertNotNull("INFO_CONF -> Non null INFO expected.", infoConf.getInfo());
+        assertEquals("INFO_CONF ->  Transmission of information initiated were expected.", Info.REQUEST_ACCEPTED,
+                infoConf.getInfo());
+    }
 
     @Test
     public void testDoNothing() {
